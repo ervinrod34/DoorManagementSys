b0fb52b4fa1532c472798dd4484f1da5554bfb86
ervinrod34 <ervin34_rodriguez@yahoo.com>
2017-12-07 20:27:58 -0600

    Fixed conflicts.
    Conflict in InventoryEditController. Re-pushing.

--- DoorManageSys/src/inventory/InventoryEditController.java
+++ DoorManageSys/src/inventory/InventoryEditController.java
@@ -1,20 +1,16 @@
 package inventory;
 
 import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import application.*;
 import applicationdialogs.InfoDialogs;
 import applicationhelper.PageTypes;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
